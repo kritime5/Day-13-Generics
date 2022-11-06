ab1c7a44ac9ed65412d838b4d022c551174d999b
Kritika <kritsme963@gmail.com>
2022-11-06 09:14:44 -0800

    Added:test float

--- src/com/bridge/generics/TestMaximum.java
+++ src/com/bridge/generics/TestMaximum.java
@@ -1,8 +1,5 @@
 package com.bridge.generics;
 
-import java.awt.*;
-import java.util.Scanner;
-
 public class TestMaximum{
     //uc1-IN 3 INTEGER FIND MAXIMUM
     public  static Integer testMax(Integer X,Integer Y,Integer Z){
@@ -17,14 +14,34 @@ public class TestMaximum{
         return max;
 
     }
+    //UC2-FIND MAXIMUM AMONG THREE FLOAT
+    public  static Float testMax1(Float A, Float B, Float C){
+        Float max=A;
+        if(B.compareTo(max)>0){
+            max=B;
+        }
+        if(C.compareTo(max)>0){
+            max=C;
+        }
+        printMax(A,B,C,max);
+        return max;
+
+    }
+
+    private static void printMax(Float a, Float b, Float c, Float max) {
+    }
 
 
     private static void printMax(Integer x, Integer y, Integer z, Integer max) {
     }
 
+
+
     public static void main(String[] args) {
         System.out.println("ENTER THE VALE OF X,Y,Z");
         System.out.println(testMax(2,9,6));
+        System.out.println(testMax1(5F, 8F, 10F));
+
     }
 
 
